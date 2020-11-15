bda1a3739a983272415366d189c911f53f7addfc
John <john@saleshorse.stream>
2020-11-15 20:41:34 +0000

    fixing?

---
+++ beta.builder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#cd /home/john/erection.digital && 
+git pull https://github.com/JHenzi/erection.digital.git master
+#git -C /home/john/erection.digital/  pull https://github.com/JHenzi/erection.digital.git master
+#cd /home/john/erection.digital/ && 
+jekyll build -d /home/john/erection.digital/_site.beta
+cp /home/john/erection.digital/_site.beta/* /usr/local/www/beta.erection.digital -r
